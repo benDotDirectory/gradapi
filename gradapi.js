ca7dda867018bd52f1ea9764c653a39be16fc8de
ben <btov14@gmail.com>
2019-08-27 22:32:51 -0400

    Update gradapi.js
    fix attempt #3

--- gradapi.js
+++ gradapi.js
@@ -115,7 +115,7 @@ module.exports = {
 // Generate a valid hex color code
 function generateHex() {
     var hex = "#";
-    while (hex.length != 7) {
+    while (hex.length !== 7) {
         hex = "#" + (Math.random()*0xFFFFFF<<0).toString(16); // some odd bug makes our hexo 5 digits (6 if you count the #)
     }
     return hex;
